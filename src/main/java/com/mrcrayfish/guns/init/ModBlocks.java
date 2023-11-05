ce573f23adbcc7924e05da93eb83e3c24691cd4f
hotakus <ttowfive@gmail.com>
2023-11-05 08:13:45 +0800

    may be useful

--- src/main/java/com/mrcrayfish/guns/init/ModBlocks.java
+++ src/main/java/com/mrcrayfish/guns/init/ModBlocks.java
@@ -11,20 +11,20 @@ import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import com.simibubi.create.content.contraptions.relays.elementary.ShaftBlock;
-import com.tterrag.registrate.util.entry.BlockEntry;
+//import com.simibubi.create.content.contraptions.relays.elementary.ShaftBlock;
+//import com.tterrag.registrate.util.entry.BlockEntry;
 
 import static com.simibubi.create.Create.ID;
 import static com.simibubi.create.Create.REGISTRATE;
 import com.simibubi.create.foundation.data.SharedProperties;
 import net.minecraft.world.level.material.MaterialColor;
-import com.simibubi.create.foundation.block.BlockStressDefaults;
+//import com.simibubi.create.foundation.block.BlockStressDefaults;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 import static com.simibubi.create.foundation.data.BlockStateGen.axisBlock;
 import static com.simibubi.create.foundation.data.BlockStateGen.simpleCubeAll;
 import com.simibubi.create.foundation.data.BlockStateGen;
 import com.simibubi.create.foundation.data.CreateRegistrate;
-import com.simibubi.create.content.contraptions.relays.elementary.BracketedKineticBlockModel;
+//import com.simibubi.create.content.contraptions.relays.elementary.BracketedKineticBlockModel;
 
 import javax.annotation.Nullable;
 import java.util.function.Function;
@@ -40,22 +40,22 @@ public class ModBlocks
 
     public static final RegistryObject<Block> WORKBENCH = register("workbench", () -> new WorkbenchBlock(Block.Properties.of(Material.METAL).strength(1.5F)));
 
-    public static final BlockEntry<ShaftBlock> SMALL_STEEL_PIPE = REGISTRATE.block("small_steel_pipe", ShaftBlock::new)
-            .properties(p -> p.color(MaterialColor.METAL).strength(0.2F))
-            .transform(BlockStressDefaults.setNoImpact())
-            .transform(pickaxeOnly())
-            .blockstate(BlockStateGen.axisBlockProvider(false))
-            .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
-            .simpleItem()
-            .register();
-    public static final BlockEntry<ShaftBlock> SMALL_IRON_PIPE = REGISTRATE.block("small_iron_pipe", ShaftBlock::new)
-            .properties(p -> p.color(MaterialColor.METAL).strength(0.2F))
-            .transform(BlockStressDefaults.setNoImpact())
-            .transform(pickaxeOnly())
-            .blockstate(BlockStateGen.axisBlockProvider(false))
-            .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
-            .simpleItem()
-            .register();
+    // public static final BlockEntry<ShaftBlock> SMALL_STEEL_PIPE = REGISTRATE.block("small_steel_pipe", ShaftBlock::new)
+    //         .properties(p -> p.color(MaterialColor.METAL).strength(0.2F))
+    //         .transform(BlockStressDefaults.setNoImpact())
+    //         .transform(pickaxeOnly())
+    //         .blockstate(BlockStateGen.axisBlockProvider(false))
+    //         .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
+    //         .simpleItem()
+    //         .register();
+    // public static final BlockEntry<ShaftBlock> SMALL_IRON_PIPE = REGISTRATE.block("small_iron_pipe", ShaftBlock::new)
+    //         .properties(p -> p.color(MaterialColor.METAL).strength(0.2F))
+    //         .transform(BlockStressDefaults.setNoImpact())
+    //         .transform(pickaxeOnly())
+    //         .blockstate(BlockStateGen.axisBlockProvider(false))
+    //         .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
+    //         .simpleItem()
+    //         .register();
     private static <T extends Block> RegistryObject<T> register(String id, Supplier<T> blockSupplier)
     {
         return register(id, blockSupplier, block1 -> new BlockItem(block1, new Item.Properties().tab(GunMod.GROUP)));
